e7d29cade3624fabf9404007181239675480ae93
xolllst <adilya.study31@gmail.com>
2022-12-27 13:40:07 +0300

    Находим макисмальный элемент массива

---
+++ giiit.py
@@ -0,0 +1,11 @@
+def find_max(a):
+    maxim = -10**8
+    for elem in a:
+        if elem > maxim:
+            maxim = elem
+    return maxim
+len = int(input("Введите длину массива:"))
+a = []
+for i in range(len):
+    a.append(int(input("Введите " + str(i+1) + " элемент массива:")))
+print("Максимум:", find_max(a))
