72a46e5790cfe87f8118eb181b63d98e349bc7cd
xolllst <adilya.study31@gmail.com>
2022-12-27 23:40:39 +0300

    .gitignore

--- giiit.py
+++ giiit.py
@@ -1,11 +1,11 @@
-def find_max(a):
-    maxim = -10**8
+def find_min(a):
+    minimum = 10**8
     for elem in a:
-        if elem > maxim:
-            maxim = elem
-    return maxim
+        if elem < minimum:
+            minimum = elem
+    return minimum
 len = int(input("Введите длину массива:"))
 a = []
 for i in range(len):
     a.append(int(input("Введите " + str(i+1) + " элемент массива:")))
-print("Максимум:", find_max(a))
+print("Мнимум:", find_min(a))
